ce49d9cbd5439df9dab4246f7ed13a619c06d0ab
moyasser20 <mohamedyasser192023@gmail.com>
2025-09-09 22:01:54 +0300

    finish the clean arch of home screen I need to put in the UI

--- lib/modules/layout/presentation/viewModel/home_cubit.dart
+++ lib/modules/layout/presentation/viewModel/home_cubit.dart
@@ -1,30 +1,30 @@
 
-import 'package:flutter_bloc/flutter_bloc.dart';
-import 'package:injectable/injectable.dart';
+  import 'package:flutter_bloc/flutter_bloc.dart';
+  import 'package:injectable/injectable.dart';
 
-import '../../domain/usecase/get_movies_list_usecase.dart';
-import 'home_states.dart';
+  import '../../domain/usecase/get_movies_list_usecase.dart';
+  import 'home_states.dart';
 
-@injectable
-class HomeCubit extends Cubit<HomeStates>{
+  @injectable
+  class HomeCubit extends Cubit<HomeStates>{
 
-  final GetMoviesListUseCase _getMoviesListUseCase;
+    final GetMoviesListUseCase _getMoviesListUseCase;
 
-  HomeCubit(this._getMoviesListUseCase) : super(HomeInitialState());
+    HomeCubit(this._getMoviesListUseCase) : super(HomeInitialState());
 
 
-  Future<void> getMoviesList({String? genre})async{
-    emit(HomeLoadingState());
+    Future<void> getMoviesList({String? genre})async{
+      emit(HomeLoadingState());
 
-    try{
-      final result = await _getMoviesListUseCase.call(genre: genre);
+      try{
+        final result = await _getMoviesListUseCase.call(genre: genre);
 
-      emit(HomeSuccessState(result));
+        emit(HomeSuccessState(result));
+      }
+      catch(e){
+        emit(HomeErrorState(e.toString()));
+      }
     }
-    catch(e){
-      emit(HomeErrorState(e.toString()));
-    }
+
+
   }
-
-
-}
